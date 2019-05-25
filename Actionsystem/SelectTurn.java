4514138984c05ee8388445eac01a9e25d637ff80
148rashunta <ishunta148raw01f826@gmail.com>
2019-05-25 12:59:28 +0900

    make SelectTurn.java

--- Actionsystem/SelectTurn.java
+++ Actionsystem/SelectTurn.java
@@ -1,7 +1,29 @@
 package Actionsystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SelectTurn {
 	static public boolean selectturn(){
 		//黒か白かを入力させ、黒のときfalse、白のときtrueを返す。
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+
+		String str = null;
+		do{
+			System.out.println("Which color is your disk Brack or White? (b/w)?"); 
+			try{
+				str = br.readLine();
+				br.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+
+			//入力を間違えたときはもう一度やらせる
+		}while(!(str.equals("b")) && !(str.equals("w")))
+
+		if(str.equals("w")) return true;
+		return false;
 	}
 }
